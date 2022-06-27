c4ffe95f3d8c38316f3296d682616cf1a9f917ba
Amon De Shir <amonsir29@gmail.com>
2022-06-27 19:47:54 +0200

    Implement words_generate_translation_file command
    src-tauri/src
    - Implement the "words_generate_translation_file" command

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,7 +12,8 @@ fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![
       dictionary::dictionary_load_file, 
-      words::words_load_file
+      words::words_load_file,
+      words::words_generate_translation_file
     ])
     .run(context)
     .expect("error while running tauri application");
